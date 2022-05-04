c25ed56b33f5d9921d4771cd5930db365d88afb7
sumonbarai <sumonbarai78@gmail.com>
2022-05-04 15:59:06 +0600

    banner area completed

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Banner.css";
 
 const Banner = () => {
   return (
-    <div>
-      <h1>this is banner area</h1>
+    <div className="banner-area d-lg-flex justify-content-center align-items-center">
+      <div className="banner-text">
+        <h1>Inventory management software for growing businesses.</h1>
+        <Link to="inventory">
+          <button className="custom-btn">go to inventory</button>
+        </Link>
+        <Link to="login">
+          <button className="custom-btn">sign in</button>
+        </Link>
+      </div>
     </div>
   );
 };
