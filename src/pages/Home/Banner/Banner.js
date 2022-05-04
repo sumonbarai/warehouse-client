eef5cd37b039dfe78b9026890a20f0eec8522e4b
sumonbarai <sumonbarai78@gmail.com>
2022-05-04 16:11:03 +0600

    some fix for banner area

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -6,12 +6,12 @@ const Banner = () => {
   return (
     <div className="banner-area d-lg-flex justify-content-center align-items-center">
       <div className="banner-text">
-        <h1>Inventory management software for growing businesses.</h1>
+        <h1>Inventory management software for growing Businesses.</h1>
         <Link to="inventory">
           <button className="custom-btn">go to inventory</button>
         </Link>
         <Link to="login">
-          <button className="custom-btn">sign in</button>
+          <button className="custom-btn">Sign Up - It's Free.</button>
         </Link>
       </div>
     </div>
