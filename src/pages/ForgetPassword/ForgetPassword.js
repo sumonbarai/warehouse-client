3f04f74b32c62bda7ff653f3812b836117847406
sumonbarai <sumonbarai78@gmail.com>
2022-05-07 19:11:24 +0600

    password forget system apply

--- src/pages/ForgetPassword/ForgetPassword.js
+++ src/pages/ForgetPassword/ForgetPassword.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { useSendPasswordResetEmail } from "react-firebase-hooks/auth";
+import toast from "react-hot-toast";
+import auth from "../../firebase.init";
 
 const ForgetPassword = () => {
+  const [sendPasswordResetEmail] = useSendPasswordResetEmail(auth);
+  const handleForgetPassword = async (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    await sendPasswordResetEmail(email);
+    toast.success("please check email for password reset link");
+    event.target.reset();
+  };
+
   return (
     <div className="login-area">
       <div className="container">
         <div className="user">
           <div className="form-area">
             <h4>Forget Password</h4>
-            <form>
+            <form onSubmit={handleForgetPassword}>
               <input type="email" name="email" placeholder="Enter Email " />
               <input type="submit" value="Forget Password" />
             </form>
